5a59051fd9d05222355e4f91be4e7b71260e523c
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2021-11-01 00:16:18 +0530

    final commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,156 @@
+import 'package:bmi_calculator/constants/app_constants.dart';
+import 'package:bmi_calculator/widgets/left_bar.dart';
+import 'package:bmi_calculator/widgets/right_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController _height = TextEditingController();
+  TextEditingController _weight = TextEditingController();
+  double _result = 0;
+  String _textResult = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BMI Calculator',
+          style: TextStyle(color: accentHexColor, fontWeight: FontWeight.w300),
+        ),
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      backgroundColor: mainHexColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 130,
+                  child: TextField(
+                    controller: _height,
+                    style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.w300,
+                        color: accentHexColor),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Height',
+                      hintStyle: TextStyle(
+                          fontSize: 42,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white.withOpacity(.8)),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 130,
+                  child: TextField(
+                    controller: _weight,
+                    style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.w300,
+                        color: accentHexColor),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Weight',
+                      hintStyle: TextStyle(
+                          fontSize: 42,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white.withOpacity(.8)),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: () {
+                double _h = double.parse(_height.text);
+                double _w = double.parse(_weight.text);
+                setState(() {
+                  _result = _w / (_h * _h);
+                  if (_result > 25) {
+                    _textResult = "Over Weight";
+                  } else if (_result >= 18.5 && _result <= 25) {
+                    _textResult = "Normal Weight";
+                  } else {
+                    _textResult = "Under Weight";
+                  }
+                });
+              },
+              child: Text(
+                'Calculate',
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: accentHexColor),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              child: Text(
+                _result.toStringAsFixed(2),
+                style: TextStyle(fontSize: 90, color: accentHexColor),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Visibility(
+              visible: _textResult.isNotEmpty,
+              child: Container(
+                child: Text(
+                  _textResult,
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w400,
+                      color: accentHexColor),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            LeftBar(barWidth: 40),
+            SizedBox(
+              height: 20,
+            ),
+            LeftBar(barWidth: 70),
+            SizedBox(
+              height: 20,
+            ),
+            LeftBar(barWidth: 40),
+            SizedBox(
+              height: 20,
+            ),
+            RightBar(barWidth: 70),
+            SizedBox(
+              height: 50,
+            ),
+            RightBar(barWidth: 70),
+          ],
+        ),
+      ),
+    );
+  }
+}
